25039894248d83f1ae646b44006f70c2d982a541
alijata123 <alijataaa@gmail.com>
2017-02-05 15:05:02 +0200

    Added NgChatApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAeRIGOeYSXuvlp-rw6jt4Q0XReh2Fut6o',
+  authDomain: 'chatapp-dc29d.firebaseapp.com',
+  databaseURL: 'https://chatapp-dc29d.firebaseio.com',
+  storageBucket: 'chatapp-dc29d.appspot.com',
+  messagingSenderId: '184044674679'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
